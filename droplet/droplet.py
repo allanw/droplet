43833b0291e5e79a662d55c2c61f432531a76456
Allan Whatmough <al.whatmough@gmail.com>
2012-12-26 21:17:24 +0000

    Add blog post listing page

--- droplet/droplet.py
+++ droplet/droplet.py
@@ -1,21 +1,50 @@
-from bottle import route, redirect, request, abort
+from bottle import route, redirect, request, abort, template, TEMPLATE_PATH
 from dropbox import Dropbox
 import os
+import markdown
+
+TEMPLATE_PATH.append('./droplet/templates')
 
 dropbox = Dropbox()
 dropbox.app_key = os.environ.get('DROPBOX_APP_KEY')
 dropbox.app_secret = os.environ.get('DROPBOX_APP_SECRET')
 
+def get_markdown():
+    return markdown.Markdown(extensions=[
+        "meta", "extra", "codehilite", "headerid(level=2)",
+        "sane_lists",
+    ])
+
+def listing():
+    files = dropbox.client.search("/", ".md")
+    posts = []
+    for f in files:
+        src = dropbox.read_file(f['path'])
+        mdown = get_markdown()
+        html = mdown.convert(src)
+        if "title" in mdown.Meta and "date" in mdown.Meta:
+            posts.append({
+                "path": f["path"][:-3],
+                "title": mdown.Meta["title"][0],
+                "date": mdown.Meta["date"][0],
+                "html": html
+            })
+    return posts
+
+@route('/')
+@dropbox.connected
+def index():
+    return template('list', posts=listing())
+
 @route('/<pagename>/')
 @dropbox.connected
-def index(pagename):
-    post_listing = listing()
+def page(pagename):
+    post_listing = dropbox.client.search("/", ".md")
     url_listing = [p["path"] for p in post_listing]
     if "/" + pagename + ".md" not in url_listing:
         abort(404, "File not found")
     post_index = url_listing.index("/" + pagename + ".md")
     src = dropbox.read_file(pagename + ".md")
-    return 'hello world!!!'
-
-def listing():
-    return dropbox.client.search("/", ".md")
+    mdown = get_markdown()
+    html = mdown.convert(src)
+    return template('post', body=html)
