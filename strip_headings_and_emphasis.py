0f27d88355d055bb34268429bd37c682c3423ea3
Allan Whatmough <al.whatmough@gmail.com>
2016-05-05 15:03:31 +0100

    Add underlines beneath headings on plaintext CV

--- strip_headings_and_emphasis.py
+++ strip_headings_and_emphasis.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
-from pandocfilters import walk, toJSONFilter, Str, Emph, Para
+from pandocfilters import walk, toJSONFilter, Str, Emph, Para, stringify
 from caps import caps
 
 def strip_headings_and_emphasis(key, val, fmt, meta):
     if key == 'Strong' or key == 'Emph':
         return walk(val, caps, fmt, meta)
+    if key == 'Header' and len(val[2]):
+        return Para([Emph(val[2])] + [Str('\n')] + [Str('=')]*len(stringify(val)))
 
 if __name__ == "__main__":
     toJSONFilter(strip_headings_and_emphasis)
