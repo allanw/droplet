2f471174146bce689ee42278bddbd10d6c306656
allanw <al.whatmough@gmail.com>
2020-11-05 10:38:31 +1100

    update mp3 download code

--- droplet/droplet.py
+++ droplet/droplet.py
@@ -195,10 +195,18 @@ def cv():
 @route('/foo')
 def foo():
   response.content_type = 'application/json'
-#   resp = {"french": {"url": "http://allanwblog-heroku-18.herokuapp.com/foo/french.mp3", "type": "mp3", "canSkip": "true", "id": "ded84ffd4b8b47c9755652c2143c15cd", "name": "french"}}
-  resp = {"shoedog": {"url": "http://www.dropbox.com/s/1rmkgn1vu244q49/shoedog.mp3?dl=1", "type": "mp3", "canSkip": "true", "id": "ded84ffd4b8b47c9755652c2143c15cg", "name": "shoedog"}}
+  resp = {"piano": {"url": "http://allanwblog-heroku-18.herokuapp.com/foo/piano2-CoolEdit.mp3", "type": "mp3", "canSkip": "true", "id": "ded84ffd4b8b47c9755652c2143c15ce", "name": "piano"}}
+#  resp = {"shoedog": {"url": "http://www.dropbox.com/s/1rmkgn1vu244q49/shoedog.mp3?dl=1", "type": "mp3", "canSkip": "true", "id": "ded84ffd4b8b47c9755652c2143c15cg", "name": "shoedog"}}
   return json.dumps(resp)
 
 @route('/foo/<filepath:path>')
 def french(filepath):
-  return static_file(filepath, root='droplet/static/')
+  client = get_client()
+  files = client.files_search('', '.mp3')
+  for file in files.matches:
+    if file.metadata.name == 'piano2-CoolEdit.mp3':
+      metadata, res = client.files_download(file.metadata.path_lower)
+      response.content_type = 'audio/mpeg'
+      response.set_header('Content-Disposition', 'attachment; filename="piano2-CoolEdit.mp3"')
+      return res.content
+  #return static_file(filepath, root='droplet/static/')
